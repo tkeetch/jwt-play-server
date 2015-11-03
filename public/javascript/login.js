307adb719117bc4f48668db714308a79e6641dcb
Tom Keetch <tom@tkeetch.co.uk>
2015-11-03 18:32:21 +0000

    Fix jslint warnings

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,7 @@
 //JavaScript functions for /login
 
+"use strict";
+
 function authServiceRequest(url, payload, onSuccess, onError) {
   $.ajax({
     type: 'POST',
@@ -10,23 +12,38 @@ function authServiceRequest(url, payload, onSuccess, onError) {
     jsonp: false,
     success: onSuccess,
     error: onError
-  })
+  });
 }
 
 function getRelativeUrl(action) {
-  return '/' + action + '.json';
+  return ('/' + action + '.json');
 }
 
 function authenticate(path, userid, credential, onSuccess, onError) {
   authServiceRequest(
     getRelativeUrl(path),
-	{ "userid": userid, "credential": credential },
+    { "userid": userid, "credential": credential },
     onSuccess,
-	onError);
+    onError);
 }
 
-function authenticationSuccessCallback(responseData, textStatus, jqXHR) {
-  $("#errorMsg").html("")
+function decodeToken(jwt) {
+  var b64Claims = jwt.split('.')[1];
+  if(b64Claims !== undefined)
+  {
+    var jsonClaims = atob( b64Claims );
+    return jsonClaims.replace(/\{/g, '\{\n  ').replace(/,"/g, ',\n  "').replace(/\}/g, '\n\}');
+  }
+  return '';
+}
+
+function decodeNewTokens() {
+  $("#decodedAuthToken").val(decodeToken($("#authToken").val()));
+  $("#decodedRefreshToken").val(decodeToken($("#refreshToken").val()));
+}
+
+function authenticationSuccessCallback(responseData) {
+  $("#errorMsg").html("");
   $("#authToken").val(responseData.authToken);
   $("#refreshToken").val(responseData.refreshToken);
   $("#csrfToken").val(responseData.csrfToken);
@@ -35,60 +52,45 @@ function authenticationSuccessCallback(responseData, textStatus, jqXHR) {
 }
 
 function loginFailure(msg) {
-  $("#errorMsg").text(msg).css("color","red")
+  $("#errorMsg").text(msg).css("color","red");
   $("#authToken").val("");
   $("#refreshToken").val("");
   $("#csrfToken").val("");
   $("#decodedAuthToken").val("");
   $("#decodedRefreshToken").val("");
-
 }
 
-function loginFailureCallback(responseData, textStatus, errorThrown) {
+function loginFailureCallback() {
   loginFailure("Login Failed!");
 }
 
-function refreshFailureCallback(responseData, textStatus, errorThrown) {
+function refreshFailureCallback() {
   loginFailure("Token Refresh Failed!");
-}
-
-function decodeToken(jwt) {
-  var b64Claims = jwt.split('.')[1];
-  if(b64Claims != undefined)
-  {
-    var jsonClaims = atob( b64Claims );
-    return jsonClaims.replace(/\{/g, '\{\n  ').replace(/,"/g, ',\n  "').replace(/\}/g, '\n\}')
-  }
-  return '';
-}
-
-function decodeNewTokens() {
-  $("#decodedAuthToken").val(decodeToken($("#authToken").val()))
-  $("#decodedRefreshToken").val(decodeToken($("#refreshToken").val()))
 }
 
 function submitLoginOnClick() {
   authenticate('login',
-               $("#userid").val(),
-			   $("#password").val(),
-			   authenticationSuccessCallback,
-			   loginFailureCallback);
+    $("#userid").val(),
+    $("#password").val(),
+    authenticationSuccessCallback,
+    loginFailureCallback);
 }
 
 function submitRefreshOnClick() {
   authenticate('refresh',
-               $("#userid").val(),
-			   $("#refreshToken").val(),
-			   authenticationSuccessCallback,
-			   refreshFailureCallback);
+    $("#userid").val(),
+    $("#refreshToken").val(),
+    authenticationSuccessCallback,
+    refreshFailureCallback);
 }
 
 function init() {
   //Register onClick Handler for Login
-  $("#loginButton").click(submitLoginOnClick)
+  $("#loginButton").click(submitLoginOnClick);
 
   //Register onClick Handler for Token Refresh
-  $("#refreshButton").click(submitRefreshOnClick)
+  $("#refreshButton").click(submitRefreshOnClick);
 }
 
-init()
+init();
+
